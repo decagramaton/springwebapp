820f2b95caad9e846a3eb65eabcadcb141d5cd18
decagramaton <csjs.go@gmail.com>
2023-07-18 10:25:13 +0900

    [REMOVE] Log

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -12,9 +12,6 @@ public class HomeController{
 	@RequestMapping("/")	// http://localhost:8080/sptringwebapp/
 	public String index() {
 		log.info("실행1");
-		log.info("실행2");
-		log.info("실행3");
-		log.info("실행4");
 		return "index";
 	}
 }
