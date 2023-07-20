7c515fbec17b15af652d99086abfc1020f2f0454
decagramaton <csjs.go@gmail.com>
2023-07-20 16:07:16 +0900

    Controller Validation(유효성 검증) 실습 init

---
+++ src/main/java/com/mycompany/springwebapp/controller/Ch04Controller.java
@@ -0,0 +1,31 @@
+package com.mycompany.springwebapp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mycompany.springwebapp.dto.Ch03Dto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/ch04")
+public class Ch04Controller {
+	@RequestMapping("/content")
+	public String content() {
+		return "ch04/content";
+	}
+	
+}
