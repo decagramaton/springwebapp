cc8c9bdba6b7d8d68e0d39472a169889352f4b09
decagramaton <csjs.go@gmail.com>
2023-07-24 09:36:37 +0900

    Get Request Header Value 실습코드

--- src/main/java/com/mycompany/springwebapp/controller/Ch05Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch05Controller.java
@@ -1,6 +1,10 @@
 package com.mycompany.springwebapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,5 +17,22 @@ public class Ch05Controller {
 	@RequestMapping("/content")
 	public String content() {
 		return "ch05/content";
+	}
+	
+	@GetMapping("/getHeaderValue")
+	public String getHeaderValue(HttpServletRequest request, @RequestHeader("User-Agent") String userAgent) {
+		
+		log.info(request.getRemoteAddr());
+		log.info(userAgent);
+		
+		
+		return "redirect:/ch05/content";
+	}
+	
+	
+	@GetMapping("/createCookie")
+	public String createCookie() {
+		
+		return "redirect:/ch05/content";
 	}
 }
--- src/main/webapp/WEB-INF/views/ch05/content.jsp
+++ src/main/webapp/WEB-INF/views/ch05/content.jsp
@@ -1,15 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
-
-
 <%@ include file="/WEB-INF/views/common/header.jsp" %>
 
 <div class="card m-2">
-	<div class="card-header">
-	   Ch05. 요청 헤더 얻기 및 쿠키
-	</div>
+	<div class="card-header">Ch05. 요청 헤더 얻기 및 쿠키 사용</div>
 	<div class="card-body">
-		
+		<div class="m-2">
+			<a href="getHeaderValue" class="btn btn-info btn-sm">요청 헤더 값 얻기</a>
+		</div>
+		<div class="m-2">
+			<a href="createCookie" class="btn btn-info btn-sm">Cookie 생성</a>
+		</div>
 	</div>
 </div>
 
