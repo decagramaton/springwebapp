37c759630b4730a4daf47d5a114a1560b8f981d2
decagramaton <csjs.go@gmail.com>
2023-07-24 16:12:03 +0900

    Ch08 Session 기반 객체 load, delete

--- src/main/java/com/mycompany/springwebapp/controller/Ch08Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch08Controller.java
@@ -1,7 +1,18 @@
 package com.mycompany.springwebapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.springwebapp.dto.Ch08Item;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,5 +23,45 @@ public class Ch08Controller {
 	@RequestMapping("/content")
 	public String content() {
 		return "ch08/content";
+	}
+	
+	@PostMapping("/addCart")
+	public String addCart(Ch08Item item, HttpSession session) {
+		// get Cart Object in Session
+		List<Ch08Item> cart = (List<Ch08Item>) session.getAttribute("cart");
+		
+		// 세션에 카드가 없을 경우, 새 객체를 세션에 저장
+		if(cart == null) {
+			cart = new ArrayList<Ch08Item>();
+			session.setAttribute("cart", cart);
+		}
+		
+		boolean exist = false;
+		for(Ch08Item cartItem : cart) {
+			// 카드에 기존 아이템이 있는 경우, 수량 수정
+			if(cartItem.getName().equals(item.getName())) {
+				cartItem.setAmount(cartItem.getAmount() + item.getAmount());
+				exist = true;
+			}
+		}
+		
+		// 카드에 없는 새로운 아이템이 있는 경우.
+		if(exist == false) {
+			cart.add(item);
+		}
+		
+		return "redirect:/ch08/content";
+	}
+	
+	@GetMapping("/clearCart")
+	public String clearCart(HttpSession session) {
+		// 세션 삭제 방법1. 세션에 저장된 객체 삭제
+		session.removeAttribute("cart");
+		
+		// 세션 삭제 방법2. 세션 자체를 무효화(세션에 저장된 모든 정보 소멸)
+		// 현 HttpSession의 객체는 삭제, 다음 요청시 새로운 세션 객체 생성
+		//session.invalidate();
+		
+		return "redirect:/ch08/content";
 	}
 }
--- src/main/webapp/WEB-INF/views/ch08/content.jsp
+++ src/main/webapp/WEB-INF/views/ch08/content.jsp
@@ -1,16 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ include file="/WEB-INF/views/common/header.jsp" %>
 
 <div class="card m-2">
-	<div class="card-header">Ch08. Session을 활용한 모델(데이터) 전달</div>
+	<div class="card-header">Ch08. Session 이동</div>
 	<div class="card-body">
-		<div>
-			<a href="useSeesion1" class="btn btn-info btn-sm">Seesion1 이용</a>
-			<a href="useSeesion2" class="btn btn-info btn-sm">Seesion2 이용</a>
-			<a href="useSeesion3" class="btn btn-info btn-sm">Seesion3 이용</a>
-			<a href="useSeesion4" class="btn btn-info btn-sm">Seesion4 이용</a>
-			<a href="useSeesion5" class="btn btn-info btn-sm">Seesion5 이용</a>
-		</div>
+		<form method="post" action="addCart">
+			<div class="form-group">
+		    	<label for="name">상품선택:</label>
+		    	<select class="form-control" id="name"name="name">
+		    		<option value="item1" selected="selected">아이템1</option>
+					<option value="item2">아이템2</option>
+					<option value="item3">아이템3</option>
+					<option value="item4">아이템4</option>
+					<option value="item5">아이템5</option>
+		    	</select>
+		  	</div>
+		  	<div class="form-group">
+		    	<label for="amount">수량:</label>
+		    	<input type="number" id="amount" name="amount" value="1"/>
+		  	</div>
+		  	<button type="submit" class="btn btn-info btn-sm m-2">장바구니 넣기</button>
+		</form>
+		
+		<a href="clearCart" class="btn btn-info btn-sm m-2">장바구니 비우기</a>
+		
+		<hr>
+		<p>장바구니 내용</p>
+		<ul>
+			<c:forEach var="item" items="${cart}">
+				<li>${item.name}, ${item.amount}개</li>
+			</c:forEach>
+		</ul>
 	</div>
 </div>
 
