5bc114c9942f0fa6fc77b8901a5dfd678f59fc5d
decagramaton <csjs.go@gmail.com>
2023-07-24 09:55:25 +0900

    Create Cookie practice

--- src/main/java/com/mycompany/springwebapp/controller/Ch05Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch05Controller.java
@@ -1,11 +1,14 @@
 package com.mycompany.springwebapp.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,8 +33,21 @@ public class Ch05Controller {
 	}
 	
 	
-	@GetMapping("/createCookie")
-	public String createCookie() {
+	@RequestMapping(value="/createCookie", method=RequestMethod.GET)
+	public String createCookie(HttpServletResponse response) {
+		
+		Cookie cookie = new Cookie("useremail", "summer@mycompany.com");
+		cookie.setDomain("localhost");
+		cookie.setPath("/");
+		cookie.setMaxAge(30*60);
+		
+		// JavaScript에서 쿠키 정보 읽기 가능 여부 설정.
+		// 즉, HTTP로 요청 시에만 쿠키 데이터 사용 가능
+		cookie.setHttpOnly(true);
+		
+		cookie.setSecure(false);
+		response.addCookie(cookie);
+		
 		
 		return "redirect:/ch05/content";
 	}
