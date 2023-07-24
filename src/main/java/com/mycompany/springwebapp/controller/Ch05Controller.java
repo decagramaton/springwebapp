8391b11489e1534fe73dc12580b4b657695b7a1d
decagramaton <csjs.go@gmail.com>
2023-07-24 09:16:38 +0900

    Spring Ch05. Get Request Header & Cookie 실습 구축

---
+++ src/main/java/com/mycompany/springwebapp/controller/Ch05Controller.java
@@ -0,0 +1,17 @@
+package com.mycompany.springwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/ch05")
+public class Ch05Controller {
+	
+	@RequestMapping("/content")
+	public String content() {
+		return "ch05/content";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/ch05/content.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
+
+<%@ include file="/WEB-INF/views/common/header.jsp" %>
+
+<div class="card m-2">
+	<div class="card-header">
+	   Ch05. 요청 헤더 얻기 및 쿠키
+	</div>
+	<div class="card-body">
+		
+	</div>
+</div>
+
+<%@ include file="/WEB-INF/views/common/footer.jsp" %>
