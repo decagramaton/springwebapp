c2cdca496d4f63229c9c2090b672e0694c7a01a9
decagramaton <csjs.go@gmail.com>
2023-07-25 17:16:34 +0900

    ch11. Spring Tag Library 실습 <form:input>, <form:password>, <form:hidden>

--- src/main/java/com/mycompany/springwebapp/controller/Ch11Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch11Controller.java
@@ -1,7 +1,11 @@
 package com.mycompany.springwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.springwebapp.dto.Ch11Member;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +16,15 @@ public class Ch11Controller {
 	@RequestMapping("/content")
 	public String content() {
 		return "ch11/content";
+	}
+	
+	@GetMapping("/form1")
+	public String form1(@ModelAttribute("member") Ch11Member member) {
+		member.setMid("summber");
+		member.setMname("홍길동");
+		member.setMpassword("12345");
+		member.setMnation("한국");
+		return "ch11/form1";
 	}
 
 }
